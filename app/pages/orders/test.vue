a5051780f2b60c71ae9c495b2e0bbf000f6cc0f1
Ahmed Elselly <ahmedelselly87@gmail.com>
2026-08-19 15:04:10 +0300

    added a test file in pages

---
+++ app/pages/orders/test.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data: orders, pending, error } = await useFetch("/api/orders");
+</script>
+
+<template>
+  <h1>Orders</h1>
+
+  <p v-if="pending">Loading...</p>
+
+  <p v-else-if="error">Failed to load orders.</p>
+
+  <div v-else>
+    <div v-for="order in orders" :key="order.id">
+      {{ order.customer }}
+    </div>
+  </div>
+</template>
