64d64c068037cff330bbfc0501fc83c975bf0a4d
Ahmed Elselly <ahmedelselly87@gmail.com>
2026-08-19 11:37:18 +0300

    added useAsyncData

--- app/composables/userOrders/index.ts
+++ app/composables/userOrders/index.ts
@@ -6,12 +6,20 @@ export function useOrders() {
     error,
     pending,
     refresh: refreshOrders,
-  } = useFetch<Order[] | null>("/api/orders", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
+  } = useAsyncData<Order[] | null>("/api/orders", () => {
+    return $fetch("/api/orders");
   });
+  // const {
+  //   data: orders,
+  //   error,
+  //   pending,
+  //   refresh: refreshOrders,
+  // } = useFetch<Order[] | null>("/api/orders", {
+  //   method: "GET",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
 
   const cancelled = ref(2);
 
